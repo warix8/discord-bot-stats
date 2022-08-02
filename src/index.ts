7ff5e9ddcc57e5950a34dc4cd45d009e678b3508
Warix <39554785+warix8@users.noreply.github.com>
2022-08-02 19:01:42 +0200

    🚀 Try first version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 module.exports.BaseGraph = require("./graphs/BaseGraph");
 module.exports.StatsManager = require("./StatsManager");
+module.exports.StatsModule = require("./StatsModule");
 module.exports.DoughnutGraph = require("./graphs/DoughnutGraph");
 module.exports.LineGraph = require("./graphs/LineGraph");
 module.exports.BarGraph = require("./graphs/BarGraph");

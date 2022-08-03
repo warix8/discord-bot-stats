0ff7e8d892b8d92745c0dba4b2ddb2458e4d0fca
Warix <39554785+warix8@users.noreply.github.com>
2022-08-03 12:03:39 +0200

    🚧 Documentation

--- README.md
+++
@@ -1,2 +0,0 @@
-# discord-bot-stats
-A Node.js package to have statistics graphs for your Discord Bot
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export { GraphBase } from "./graphs/BaseGraph";
-export { StatsManager } from "./StatsManager";
-export { StatsModule } from "./StatsModule";
-export { DoughnutGraph } from "./graphs/DoughnutGraph";
-export { LineGraph } from "./graphs/LineGraph";
-export { BarGraph } from "./graphs/BarGraph";
